0a3c221025e3f20a286e6c8c4903dfac1a1d4a9b
AsusOnePlus X <onex@AsusOnePlusX.local>
2020-08-11 16:29:31 -0500

    solving exercise

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value:'',
+    counter: 0
+  }
+  
+  handleChange = (event) => {
+    
+    this.setState({
+      value: event.target.value,
+      counter: event.target.value.length
+    })
+    
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea onChange={this.handleChange} rows="3"></textarea>
+        <div className="counter">{ this.state.counter }</div>
       </div>
     );
   }
